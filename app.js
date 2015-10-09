9c11c4572f3ba4c979ce71c1b4abcac277c1e303
Paul Diederichs <pauldiederichs95@gmail.com>
2015-10-08 20:00:19 -0400

    testing routes

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-//Modules
+// =-=-=-=-=-=-=-=-=-=-=- Modules -=-=-=-=-=-=-=-=-=-=-=-=
 var express = require('express'),
-	bodyParser = require('body-parser'),
 	app = express(),
 	exphbs  = require('express-handlebars');
+
+// =-=-=-=-=-=-=-=-=-=-=- View engine -=-=-=-=-=-=-=-=-=-=
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + '/public'));
+
+// =-=-=-=-=-=-=-=-=-=-=- Config vars -=-=-=-=-=-=-=-=-=-=
+var port = process.env.PORT || 8080;
+
+// =-=-=-=-=-=-=-=-=-=-=- Routes -=-=-=-=-=-=-=-=-=-=-=-=-=
+require('./routes/routes')(app);
+
+app.listen(port);
+console.log("app running on port",port);
---
+++ routes/routes.js
@@ -0,0 +1,12 @@
+module.exports = function(app) {
+
+var bodyParser = require('body-parser');
+// create application/x-www-form-urlencoded parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+    app.get('/',function(req,res){
+        res.render('home');
+    });
+}//end export
