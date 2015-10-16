5f94ced31b46f1f02db95a8afe273758b8583e5d
Paul Diederichs <pauldiederichs95@gmail.com>
2015-10-16 09:23:58 -0400

    fixed loading angular errors

--- public/js/materialize.js
+++ public/js/materialize.js
@@ -1,6 +1,5 @@
-
 $(document).ready(function() {
-	
+
 });
 $(document).ready(function() {
 	console.log("hello world");
@@ -790,15 +789,15 @@ if ($) { Vel = $.Velocity } else { Vel = Velocity};
       var originalWidth = 0;
       var originalHeight = 0;
       origin.wrap(placeholder);
-      
-      
+
+
       origin.on('click', function(){
         var placeholder = origin.parent('.material-placeholder');
         var windowWidth = window.innerWidth;
         var windowHeight = window.innerHeight;
         var originalWidth = origin.width();
         var originalHeight = origin.height();
-        
+
 
         // If already modal, return to original
         if (doneAnimating === false) {
@@ -809,7 +808,7 @@ if ($) { Vel = $.Velocity } else { Vel = Velocity};
           returnToOriginal();
           return false;
         }
-        
+
 
         // Set states
         doneAnimating = false;
@@ -825,8 +824,8 @@ if ($) { Vel = $.Velocity } else { Vel = Velocity};
           top: 0,
           left: 0
         });
-        
-        
+
+
 
         // Set css on origin
         origin.css({position: 'absolute', 'z-index': 1000})
@@ -949,7 +948,7 @@ if ($) { Vel = $.Velocity } else { Vel = Velocity};
           var windowHeight = window.innerHeight;
           var originalWidth = origin.data('width');
           var originalHeight = origin.data('height');
-       
+
           origin.velocity("stop", true);
           $('#materialbox-overlay').velocity("stop", true);
           $('.materialbox-caption').velocity("stop", true);
@@ -2347,9 +2346,9 @@ $(document).ready(function(){
 		    var offset = $(this.hash).offset().top + 1;
 
 //          offset - 200 allows elements near bottom of page to scroll
-			
+
 	    	$('html, body').animate({ scrollTop: offset - 200 }, {duration: 400, queue: false, easing: 'easeOutCubic'});
-			
+
 		  });
 		});
 		options = options || {
@@ -6063,5 +6062,3 @@ $('.modal-trigger').leanModal({
       out_duration: 200, // Transition out duration
     }
   );
-
-
