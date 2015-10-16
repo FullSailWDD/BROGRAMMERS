8f2a056ae5a7b814c3b6965a93f1493a3076f7da
Paul Diederichs <pauldiederichs95@gmail.com>
2015-10-15 21:37:52 -0400

    cleaned john

--- gulpfile.js
+++
@@ -1,21 +0,0 @@
-var
-    gulp            = require('gulp'), 
-    child_process   = require('child_process'),
-    nodemon         = require('gulp-nodemon');
-
-// startup required services to run the app server
-gulp.task('mongod', function() { 
-    // spawn in a child process mongodb
-    child_process.exec('mongod', function(err,stdout,stderr){
-    	console.log(stdout);
-    });
-});
-
-gulp.task('dev', function () {
-  nodemon({ script: 'app.js'
-          , ext: 'js' }).on('restart', function () {
-      console.log('restarted!')
-    })
-});
-
-  gulp.task('startup', ['mongod', 'dev']);
